db516381cbe3d33f613a8e0347ddacfe5309a7ab
Jerryfunny <158678051+Jerryfunny@users.noreply.github.com>
2025-12-02 17:02:44 +0800

    Delete User.java

--- User.java
+++
@@ -1,9 +0,0 @@
-@Entity
-@Table(name="users")
-public class User {
-@Id @GeneratedValue private Long id;
-private String username;
-private String password; // 存储 bcrypt
-private String role; // ROLE_USER / ROLE_ADMIN
-private LocalDateTime createTime;
-}
