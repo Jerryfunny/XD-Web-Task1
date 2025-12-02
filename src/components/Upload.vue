ffe56063df960b776dd36513aab1b9ea71dfc329
Jerryfunny <158678051+Jerryfunny@users.noreply.github.com>
2025-12-02 16:46:47 +0800

    Add files via upload

---
+++ src/components/Upload.vue
@@ -0,0 +1,19 @@
+<template>
+<div>
+<input type="file" @change="onFileChange" />
+<button @click="upload">上传</button>
+</div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+data(){return {file:null}},
+methods:{
+onFileChange(e){this.file=e.target.files[0]},
+async upload(){
+const fd=new FormData(); fd.append('file', this.file);
+await axios.post('/api/docs/upload', fd, {headers:{'Content-Type':'multipart/form-data'}})
+}
+}
+}
+</script>
